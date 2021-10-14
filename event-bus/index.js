1e68b2f4710423131b68572715b62c086b6c121f
Marc Mendoza <777marc@gmail.com>
2021-10-14 13:21:20 -0400

    wiring

--- event-bus/index.js
+++ event-bus/index.js
@@ -12,13 +12,13 @@ app.post('/events', (req, res) => {
     const event = req.body;
 
     // posts
-    axios.post('http://localhost:4000/events', event);
+    axios.post('http://posts-clusterip-svr:4000/events', event);
     //comments
-    axios.post('http://localhost:4001/events', event);
-    // query
-    axios.post('http://localhost:4002/events', event);
-    // moderation
-    axios.post('http://localhost:4003/events', event);
+    // axios.post('http://localhost:4001/events', event);
+    // // query
+    // axios.post('http://localhost:4002/events', event);
+    // // moderation
+    // axios.post('http://localhost:4003/events', event);
 
     res.send({ status: 'OK' });
 
