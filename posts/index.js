df6ee440425a4490865c62f9c10da3a20f1b92be
Marc Mendoza <777marc@gmail.com>
2021-10-22 17:55:49 -0400

    misc

--- posts/index.js
+++ posts/index.js
@@ -1,46 +1,46 @@
 const express = require('express');
-const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
 const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
 
 app.get('/posts', (req, res) => {
-    res.send(posts);
+  res.send(posts);
 });
 
 app.post('/posts/create', async (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
 
-    console.log('in post');
+  posts[id] = {
+    id,
+    title
+  };
 
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
-
-    posts[id] = {
-        id, title
+  await axios.post('http://event-bus-srv:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title
     }
+  });
 
-    await axios.post('http://event-bus-svr:4005/events', {
-        type: 'PostCreated',
-        data: {
-            id, title
-        }
-    });    
-
-    res.status(201).send(posts[id]);
+  res.status(201).send(posts[id]);
 });
 
 app.post('/events', (req, res) => {
-    console.log('received event', req.body.type);
-    res.send({});
+  console.log('Received Event', req.body.type);
+
+  res.send({});
 });
 
 app.listen(4000, () => {
-    console.log('listening on port 4000 ...');
-}); 
+  console.log('v55');
+  console.log('Listening on 4000');
+});
